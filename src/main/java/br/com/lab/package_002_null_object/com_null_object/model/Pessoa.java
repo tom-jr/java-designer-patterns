30083407d4f6dd75785327189676e7ccbede977e
tom-jr <antonio.jr.dev@gmail.com>
2024-01-18 05:05:27 -0300

    #0002 Padrão de projeto Null Object

--- src/main/java/br/com/lab/package_002_null_object/com_null_object/model/Pessoa.java
+++ src/main/java/br/com/lab/package_002_null_object/com_null_object/model/Pessoa.java
@@ -1,7 +1,6 @@
 package br.com.lab.package_002_null_object.com_null_object.model;
 
 import br.com.lab.package_002_null_object.com_null_object.model.interfaces.IEndereco;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
